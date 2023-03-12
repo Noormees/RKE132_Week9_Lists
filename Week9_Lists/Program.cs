232de01e74b972e1e99c23a8fa239ab7756e46b7
Noormees <garry.lobanoff@gmail.com>
2023-03-12 19:47:06 +0200

    add week code

---
+++ Week9_Lists/Program.cs
@@ -0,0 +1,71 @@
+﻿
+// ostude nimekiri
+
+string folderPath = @"G:\Мой диск\TKTK\3 aasta\Programmeerimise alused\data";
+string fileName = "shoppingList.txt";
+string filePath = Path.Combine(folderPath, fileName);
+
+List<string> myShoppingList = new List<string>();
+
+
+
+if (File.Exists(filePath))
+{
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+else 
+{
+    File.Create(filePath).Close();
+    Console.WriteLine($"File {fileName} has been created.");
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+
+
+
+static List<string> GetItemsFromUser()  // 1 funktsioon
+{
+    List<string> userList = new List<string>(); // loome vahemalu andmestik
+
+    while (true)
+    {
+        Console.WriteLine("Add an item (add) / Exit (exit)");
+        string userChoise = Console.ReadLine();
+
+        if (userChoise == "add")
+        {
+            Console.WriteLine("Enter an item:");
+            string userItem = Console.ReadLine();
+            userList.Add(userItem); //lisame Item to userList
+        }
+        else
+        {
+            Console.WriteLine("Bye!");
+            break;
+        }
+
+    }
+    return userList;
+
+}
+
+
+static void ShowItemsFromList(List<string> someList)    // 2 funktsioon
+{
+    Console.Clear();
+
+    int listLength = someList.Count;
+    Console.WriteLine($"You have added {listLength} items to your shopping list");
+
+    int i = 1;
+    foreach (string item in someList)
+    {
+
+        Console.WriteLine($"{i}. {item}");
+        i++;
+
+    }
+}
+
+
